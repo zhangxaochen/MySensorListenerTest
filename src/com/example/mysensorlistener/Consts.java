e0e4e39a95c45da176f58ea8883648d7aa9921f4
Chen ZHANG <zhangxaochen@163.com>
2014-03-17 09:39:45 +0800

    ! 对应 huaweiProj v1.8.0, 增加 Consts.kXXX

--- src/com/example/mysensorlistener/Consts.java
+++ src/com/example/mysensorlistener/Consts.java
@@ -5,6 +5,9 @@ public class Consts {
 	public static String kPreCd="preCd";
 	public static String kUsers="users";
 	public static String kCurrentUser="currentUser";
+	public static String kCurActType="curActType";
+	public static String kCurPosition="curPosition";
+	public static String kCurAction="curAction";
 	
 	public static final double NS2S = 1.0f / 1000000000.0f;
 	public static final double MS2S = 1.0f / 1000.0f;
